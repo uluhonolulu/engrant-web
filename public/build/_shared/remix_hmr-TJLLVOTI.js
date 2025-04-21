6c2db97f1bc53410726f347ad8a208e9aa4baea3
Artem Smirnov <uluhonolulu@gmail.com>
2025-04-21 21:28:37 +0300

    Trying to fix deployment to Azure

--- public/build/_shared/remix_hmr-TJLLVOTI.js
+++
@@ -1,9 +0,0 @@
-import {
-  createHotContext
-} from "/build/_shared/chunk-FQJPM6VB.js";
-import "/build/_shared/chunk-MZF3CFPM.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export {
-  createHotContext
-};
-//# sourceMappingURL=/build/_shared/remix_hmr-TJLLVOTI.js.map
---
+++ remix.config.js
@@ -0,0 +1,19 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "esm",
+  tailwind: true,
+  postcss: true,
+  publicPath: "/",
+  assetsBuildDirectory: "public/build",
+  serverBuildPath: "build/index.js",
+  serverBuildTarget: "vercel",
+  future: {
+    v2_dev: true,
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+}; 
