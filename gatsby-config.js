7d1626f6b339cfd4b6ecdcd4a8f1c55433de8837
Artem Smirnov <uluhonolulu@gmail.com>
2025-04-22 20:18:05 +0300

    Gatsby OK fine

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: `engrant-web-gatsby`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
@@ -18,5 +18,23 @@ module.exports = {
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  },
+  {
+	resolve: `gatsby-plugin-google-gtag`,
+	options: {
+	  // Your GA4 Measurement ID(s) (you can add multiple)
+	  trackingIds: [
+		"G-NREH71232E", // Replace this with your Measurement ID!
+	  ],
+	  // Optional configuration options
+	  pluginConfig: {
+		// Puts tracking script in the <head> instead of the <body>
+		head: true,
+		// Setting this parameter is optional
+		// respectDNT: true,
+		// Avoids sending pageview hits from custom paths
+		// exclude: ["/preview/**", "/do-not-track/me/too/"],
+	  },
+	},
+  },]
 };
