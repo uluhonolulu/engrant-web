d208e05b7cc42f5171123b85ae305be7308aedc0
Artem Smirnov <uluhonolulu@gmail.com>
2025-06-26 15:39:34 +0300

    Fixed the top links

--- src/components/ngo-dash-trickle/Features.js
+++ src/components/ngo-dash-trickle/Features.js
@@ -21,7 +21,7 @@ export default function Features() {
     ];
 
     return (
-        <section data-name="features" data-file="components/Features.js" className="py-20">
+        <section id="features" data-name="features" data-file="components/Features.js" className="py-20">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="text-center mb-16">
                     <h2 className="text-4xl font-bold text-gray-900 mb-4">
--- src/components/ngo-dash-trickle/HowItWorks.js
+++ src/components/ngo-dash-trickle/HowItWorks.js
@@ -21,7 +21,7 @@ export default function HowItWorks() {
     ];
 
     return (
-        <section data-name="how-it-works" data-file="components/HowItWorks.js" className="py-20 bg-gray-50">
+        <section id="how-it-works" data-name="how-it-works" data-file="components/HowItWorks.js" className="py-20 bg-gray-50">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="text-center mb-16">
                     <h2 className="text-4xl font-bold text-gray-900 mb-4">
