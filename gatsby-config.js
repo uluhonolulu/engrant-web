d92a38134035c9b1f64800b817a13d0fc21564ac
Artem Smirnov <uluhonolulu@gmail.com>
2025-12-11 18:54:50 +0200

    Added favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,40 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/images/engrant-favicon.svg',
+        name: 'Engrant',
+        short_name: 'Engrant',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        display: 'standalone',
+        icon: 'src/images/android-chrome-512x512.png',
+        icons: [
+          {
+            src: 'src/images/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+          {
+            src: 'src/images/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+          {
+            src: 'src/images/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'src/images/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'src/images/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+        ],
       },
     },
     {
