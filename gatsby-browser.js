673c86e6f3689e73f289e4b060d3d916203123c5
Artem Smirnov <uluhonolulu@gmail.com>
2026-01-10 12:18:51 +0200

    Added Betterlytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,14 @@
 import './src/styles/global.css';
+import betterlytics from "@betterlytics/tracker";
+
+// Initialize Betterlytics when the client loads
+export const onClientEntry = () => {
+  betterlytics.init("app-mhi4jqkf");
+};
+
+// Track page views on route change
+export const onRouteUpdate = () => {
+  if (typeof betterlytics !== 'undefined' && betterlytics.page) {
+    betterlytics.page();
+  }
+};
