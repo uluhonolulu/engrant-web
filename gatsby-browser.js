dc600d0a9eef888514a3db24c44a59b91dc76bea
Artem Smirnov <uluhonolulu@gmail.com>
2026-01-11 12:48:04 +0200

    installed RB2B

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,11 @@
 import './src/styles/global.css';
 import betterlytics from "@betterlytics/tracker";
+import { initRB2B } from './src/utils/rb2b';
 
 // Initialize Betterlytics when the client loads
 export const onClientEntry = () => {
   betterlytics.init("app-mhi4jqkf");
+  initRB2B("EN4M0HLY3POM"); // Replace with your actual RB2B unique ID
 };
 
 // Track page views on route change
