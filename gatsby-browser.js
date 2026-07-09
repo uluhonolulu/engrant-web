30c306d58c87872dd2a1177f19a1ce7d194e1296
Artem Smirnov <uluhonolulu@gmail.com>
2026-07-09 20:57:40 +0300

    Add PostHog tracking integration and update environment variables

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import './src/styles/global.css';
 import betterlytics from "@betterlytics/tracker";
 import mixpanel from 'mixpanel-browser';
+import posthog from 'posthog-js';
 
 // Initialize Betterlytics when the client loads
 export const onClientEntry = () => {
@@ -15,6 +16,18 @@ export const onClientEntry = () => {
     secure_cookie: true,
     domain: '.engrant.eu'
   });
+
+  if (process.env.GATSBY_POSTHOG_KEY) {
+    posthog.init(process.env.GATSBY_POSTHOG_KEY, {
+      api_host: process.env.GATSBY_POSTHOG_HOST || 'https://eu.i.posthog.com',
+      capture_pageview: 'history_change',
+      capture_pageleave: true,
+      persistence: 'cookie',
+      cross_subdomain_cookie: true,
+      cookie_domain: '.engrant.eu',
+      debug: process.env.NODE_ENV === 'development',
+    });
+  }
 };
 
 // Track page views on route change
